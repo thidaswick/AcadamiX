dc32a5a1c925cac349cc56c8af5681841cb8e4a8
Venusha Samarasinghe <sathsaranivenusha35@gmail.com>
2025-05-17 16:39:56 +0530

    post.js

--- backend/src/main/java/com/example/pafbackend/models/Post.java
+++ backend/src/main/java/com/example/pafbackend/models/Post.java
@@ -19,10 +19,4 @@ public class Post {
     private String mediaType;
 }
 
-import lombok.Getter;
-import lombok.Setter;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.Date;
